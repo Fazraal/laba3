4a10df6dd72052488c1197dff111c9898b90aab2
Fazraal <45007979+Fazraal@users.noreply.github.com>
2018-12-17 13:41:12 +0300

    Delete Group.java

--- github/fazraal/Group.java
+++
@@ -1,9 +0,0 @@
-package com.github.fazraal;
-
-public interface Group {
-    public int getGroupNumber();
-    public void setGroupNumber(int groupNumber);
-    public String getFaculty();
-    public void setFaculty(String faculty);
-    public boolean equals(Group obj);
-}
